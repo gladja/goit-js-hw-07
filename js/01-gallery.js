6a235065d175e4484264b445744f641f6555165d
gladja <user@MacBook-Pro-user.local>
2023-06-29 20:49:06 +0300

    01 created gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryEl.addEventListener('click', onClickElementGallery);
+
+function createdElementGallery(galleryItems) {
+  return galleryItems.map(itm => {
+    return `
+   <li class='gallery__item'>
+   <a class='gallery__link' href='${itm.original}'>
+     <img
+       class='gallery__image'
+       src='${itm.preview}'
+       data-source='large-image.jpg'
+       alt='Image description'
+     />
+   </a>
+ </li>
+  `;
+  }).join('');
+}
+
+const element = createdElementGallery(galleryItems);
+// console.log(element);
+galleryEl.insertAdjacentHTML('beforeend', element);
+
+function onClickElementGallery(event) {
+  event.preventDefault();
+  if(!event.target.classList.contains('gallery__image')) {
+    return
+  }
+
+  console.log('yes');
+
+}
