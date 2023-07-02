bf69174912e4c7070c50ec6a09d0037bec17e0a9
gladja <user@MacBook-Pro-user.local>
2023-07-02 16:18:17 +0300

    refactor

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,8 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
 
-// galleryEl.addEventListener('click', onClickElementGallery);
-
 function createdElementGallery(galleryItems) {
   return galleryItems.map(itm => {
     return `
@@ -24,16 +22,6 @@ const element = createdElementGallery(galleryItems);
 // console.log(element);
 galleryEl.insertAdjacentHTML('beforeend', element);
 
-function onClickElementGallery(event) {
-  event.preventDefault();
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
-  }
-
-  openOriginalSize(galleryItems);
-  // console.dir(event);
-}
-
 let gallery = new SimpleLightbox('.gallery a',
   {
     fadeSpeed: 300,
@@ -42,19 +30,3 @@ let gallery = new SimpleLightbox('.gallery a',
     captionsData: 'alt',
     captionDelay: 250,
 });
-
-function openOriginalSize(galleryItems) {
-  // console.log('yes');
-  // let gallery = new SimpleLightbox('.gallery a', { fadeSpeed: 300 });
-  gallery.on('show.simplelightbox', function() {
-    // return galleryItems.map(itm => {
-    return `
-      <span>${galleryItems.description[0]}</span>
-      `;
-    // }).join('');
-  });
-  gallery.on('closed.simplelightbox', function() {
-
-  });
-
-}
